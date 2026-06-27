524ce22dbec41b95b3a86c3557c08707288b5407
Ali Muhtasham Javed <alimuhtashamjawi@gmail.com>
2026-06-27 08:41:51 +0500

    code updated

--- Mini projects/Student Grade Management System/main2.py
+++ Mini projects/Student Grade Management System/main2.py
@@ -1,21 +1,15 @@
-
+students = []
 def student_data():
-    try:
-        input_name = input('What is your name (in English )?')
-        # str(input) is unnecessary because input already gives string in return
-    except ValueError:
-        print('Please input in valid format')
-    try:
-        input_roll_no = int(input('What is your roll number (in digits) ?'))
-    except ValueError:
-        print('Please input in valid format')
-    try:
-        input_marks = int(input('What are your Math Marks ( in digits)?'))
-    except ValueError:
-        print('Please input in valid format')
-        
+    input_name = input('What is your name (in English )?')
+    input_roll_no = int(input('What is your roll number (in digits) ?'))
+    input_marks = int(input('What are your Math Marks ( in digits)?'))
+      
     student_dict = {}
     student_dict['Student Name'] = input_name
     student_dict['Roll Number'] = input_roll_no
     student_dict['Math Marks'] = input_marks
     return student_dict
+
+data = student_data()
+students.append(data)
+print(students)
