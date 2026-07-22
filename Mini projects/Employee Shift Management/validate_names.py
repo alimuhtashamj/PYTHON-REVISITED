81df87b666614c6f80575f42e585aec58b6a2bdb
Ali Muhtasham Javed <alimuhtashamjawi@gmail.com>
2026-07-22 11:29:40 +0500

    updated

--- Mini projects/Employee Shift Management/validate_names.py
+++ Mini projects/Employee Shift Management/validate_names.py
@@ -6,38 +6,52 @@ def name_is_valid(employee_name):
     #     print('This is not a valid name')
     #     return errors
     if employee_name.lower() == 'done':
-        return
+        return None
     if employee_name.strip() == '':
         errors.append('Empty')
         print('Add the employee name')
-        return errors
 
     # check if the string is a number
-    try:
-        float(employee_name)
-        errors.append('Numeric')
-        print('This is a number, please add a valid name')
-        return errors
-    except ValueError:
-        pass
+    else:   
+        try:
+            float(employee_name)
+            errors.append('Numeric')
+            print('This is a number, please add a valid name')
+            return errors
+        except ValueError:
+            pass
     
 
+    # for ch in employee_name:
+    #     if ch.isdigit():
+    #         errors.append('Contains digit')
+    #         print('Employee name contains a digit')
+    #         return errors
+    #     else:
+    #        if not ch.isalnum():
+    #            errors.append(employee_name)
+    #            print('Employee name has a special character')
+    #            return errors
+    #        else:
+    #            pass
     for ch in employee_name:
-        if ch.isdigit():
-            errors.append('Contains digit')
-            print('Employee name contains a digit')
-            return errors
+        if ch.isalpha():
+            continue
+        elif ch.isspace():
+            continue
+        elif ch.isdigit():
+            if 'Contains digit' not in errors:
+                errors.append('Contains digit')
+            continue
         else:
-           if not ch.isalnum():
-               errors.append(employee_name)
-               print('Employee name has a special character')
-               return errors
-           else:
-               pass
-            
-
-    print(employee_name)
-    return employee_name, errors
+            if 'Contains special character' not in errors:
+                errors.append('Contains special character')
+            continue
+        
+    if errors:
+        return errors
+    
+    return employee_name
 
 name_is_valid(name)
    
