d6f83aecde24ef8b821498bb94d265ebb2c23269
Ali Muhtasham Javed <alimuhtashamjawi@gmail.com>
2026-07-24 12:23:19 +0500

    updated

--- Mini projects/Employee Shift Management/main.py
+++ Mini projects/Employee Shift Management/main.py
@@ -2,16 +2,18 @@ from validate_names import name_is_valid
 
 employee = {}
 def caller():
-    add_employee_name = input("Add employee's name")
-    
-    employee['employee name'] = add_employee_name
-    results = name_is_valid(employee['employee name'])
-    if results is None:
-        return 
-    elif isinstance(results, list):
-        print(results)
-        return 
-    else:
-       return results
+    while True:
+        add_employee_name = input("Add employee's name")
+        
+        employee['employee name'] = add_employee_name
+        results = name_is_valid(employee['employee name'])
+        if results is None:
+            break
+        elif isinstance(results, list):
+            print(results)
+        else:
+         continue
+     
 
-caller()
+output = caller()
+print(output)
