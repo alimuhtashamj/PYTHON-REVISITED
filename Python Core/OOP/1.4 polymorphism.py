b7277d216077bddb514fa4ab07ca99507dea312f
Ali Muhtasham Javed <alimuhtashamjawi@gmail.com>
2026-08-15 10:53:34 +0500

    added

---
+++ Python Core/OOP/1.4 polymorphism.py
@@ -0,0 +1,37 @@
+class Notification:
+   
+    def send(self, message):
+        print(message)
+    
+class EmailNotification(Notification):
+    def __init__ (self, email):
+        self.email = email
+            
+    def send(self, message):
+        print(f'{self.email} send you a mail :{message}')
+    
+class SMSNotification(Notification):
+    def __init__ ( self, sms):
+        self.sms = sms 
+            
+    def send(self, message):
+        print(f'{self.sms} sends you :{message}')
+        
+class PushNotification(Notification):
+    def __init__(self, device_id):
+        self.device_id = device_id
+            
+    def send(self, message):
+        print(f'{self.device_id} sends you :{message}')
+        
+email = EmailNotification("ali@example.com")
+sms = SMSNotification("+923001234567")
+push = PushNotification("device_123")
+
+notifications = [email, sms, push]
+
+for notification in notifications:
+    notification.send('You have been credited with 500k $')
+
+        
+    
